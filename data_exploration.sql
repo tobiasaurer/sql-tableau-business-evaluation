5c8d07d83e81ecb526a1865282ffa0b5e15cce7f
tobiasaurer <tobias@aurer.de>
2022-07-02 15:07:10 +0200

    improve formatting and fix small issues

--- data_exploration.sql
+++ data_exploration.sql
@@ -1,5 +1,5 @@
 -- 1. How many orders are there in the dataset?
-SELECT COUNT(*) 
+SELECT COUNT(order_id) 
 FROM orders;
 
 -- 2. Are orders actually delivered?
@@ -10,40 +10,51 @@ FROM orders
 GROUP BY order_status;
 
 -- 3. What are trends in the userbase?
+-- Gets monthly number of orders.
 SELECT 
 	YEAR(order_purchase_timestamp) AS Year,
     MONTH(order_purchase_timestamp) AS Month,
-    COUNT(*) AS "Number of Orders"
+    COUNT(*) AS "number of orders"
 FROM orders
 GROUP BY Year, Month
 ORDER BY Year, Month;
 
+-- Gets yearly number of orders.
 SELECT 
 	YEAR(order_purchase_timestamp) AS year,
     COUNT(*) AS "orders"
 FROM orders
-GROUP BY year;
+GROUP BY year
+ORDER BY year;
 
 -- 4. How many products are in the products table?
 SELECT 
-	COUNT(DISTINCT(product_id)) AS "Number of Products"
+	COUNT(DISTINCT(product_id)) AS "number of products"
 FROM products;
 
 -- 5. Which are the categories with most products?
 SELECT 
-	product_category_name_english as prod_category,
-    COUNT(*) AS nr_of_products
+	product_category_name_english as product_category,
+    COUNT(DISTINCT(product_id)) AS nr_of_products
 FROM 
-	products as p
-		JOIN 
-	product_category_name_translation as transl ON p.product_category_name = transl.product_category_name
-GROUP BY p.product_category_name
+	products 
+JOIN product_category_name_translation as transl 
+	ON products.product_category_name = transl.product_category_name
+GROUP BY products.product_category_name
 ORDER BY nr_of_products DESC;
 
 -- 6. Have all these products been involved in orders?
+-- Grabs number of products that were involved in at least one order, and number of total products in catalog.
 SELECT 
-	COUNT(DISTINCT(product_id)) as nr_unique_products
-FROM order_items;
+	COUNT(DISTINCT(products.product_id)) as products_in_orders,
+    (
+		SELECT 
+			COUNT(DISTINCT(product_id)) as unique_products
+		FROM order_items
+	) AS total_unique_products
+FROM products
+RIGHT JOIN order_items
+	ON order_items.product_id = products.product_id;
 
 -- 7. What's the price for the most expensive and cheapest product?
 SELECT 
